e9a92cc2ba3a7b5216b2c09a179803e4dc0d6afa
AaronI45 <zs20015694@estudiantes.uv.mx>
2023-06-06 21:15:11 -0600

    Add project files.

---
+++ ServiciosLinqTutorias/AdministracionApp/ResultadoOperacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiciosLinqTutorias.AdministracionApp
+{
+    public class ResultadoOperacion
+    {
+        private string Mensaje { get; set; }
+        private bool Error { get; set; }
+        public ResultadoOperacion() { }
+    }
+}
