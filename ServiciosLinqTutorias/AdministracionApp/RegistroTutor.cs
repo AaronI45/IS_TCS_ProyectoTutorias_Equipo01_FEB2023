5b0bae2fc65a9356227bbf290f00a22a500d3471
AaronI45 <zs20015694@estudiantes.uv.mx>
2023-06-12 22:13:53 -0600

    agregar visualizacion imagen login

---
+++ ServiciosLinqTutorias/AdministracionApp/RegistroTutor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiciosLinqTutorias.AdministracionApp
+{
+    public class RegistroTutor
+    {
+        public string nombre { get; set; }
+        public string apellidoPaterno { get; set; }
+        public string apellidoMaterno { get; set; }
+        public string correoInstitucional { get; set; }
+        public long telefono { get; set; }
+        public int numeroPersonal { get; set; }
+        public int idProgramaEducativo { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
