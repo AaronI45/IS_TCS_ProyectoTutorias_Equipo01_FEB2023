f08bea95aaa67e7c1373d62e9d79c90e7516dec3
AlvaroBarradas <abf260602@outlook.com>
2023-06-16 01:14:06 -0600

    CU modificar fechas de sesión de tutoria

--- FrontendGestorTutorias/ModificarFechasSesionTutoria.xaml.cs
+++ FrontendGestorTutorias/ModificarFechasSesionTutoria.xaml.cs
@@ -24,6 +24,7 @@ namespace FrontendGestorTutorias
         {
             InitializeComponent();
             cargarCbPeriodosEscolares();
+            cbPeriodosEscolares.SelectionChanged += cbPeriodosEscolares_SelectionChanged;
         }
 
         private void btnCancelar_Click(object sender, RoutedEventArgs e)
@@ -40,7 +41,29 @@ namespace FrontendGestorTutorias
 
         private void btnGuardar_Click(object sender, RoutedEventArgs e)
         {
-
+            if(cbPeriodosEscolares_SelectionChanged != null)
+            {
+                if (evaluarFechasVacias())
+                {
+                    MessageBox.Show("Favor de seleccionar todas las fechas", "Fechas vacías", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    DateTime? primeraFecha = dpPrimeraFechaEdicion.SelectedDate;
+                    DateTime? segundaFecha = dpSegundaFechaEdicion.SelectedDate;
+                    DateTime? terceraFecha = dpTerceraFechaEdicion.SelectedDate;
+                    var confirmacion = MessageBox.Show("¿Está seguro de querer modificar estas fechas?", "Modificar fechas",
+                                               MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    if (confirmacion == MessageBoxResult.Yes)
+                    {
+                        PeriodoEscolar periodoEscolar = new PeriodoEscolar();
+                        periodoEscolar.primeraFechaTutoria = primeraFecha;
+                        periodoEscolar.segundaFechaTutoria = segundaFecha;
+                        periodoEscolar.terceraFechaTutoria = terceraFecha;
+                        modificarFechasTutorias(periodoEscolar);
+                    }
+                }
+            }
         }
 
         private async void cargarCbPeriodosEscolares()
@@ -66,5 +89,67 @@ namespace FrontendGestorTutorias
                 MessageBox.Show("No se pudo conectar con el servidor", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+        private void cbPeriodosEscolares_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                PeriodoEscolar periodoSeleccionado = (PeriodoEscolar)cbPeriodosEscolares.SelectedItem;
+                DateTime? primeraFecha = periodoSeleccionado.primeraFechaTutoria;
+                DateTime? segundaFecha = periodoSeleccionado.segundaFechaTutoria;
+                DateTime? terceraFecha = periodoSeleccionado.terceraFechaTutoria;
+
+                tbPrimeraFecha.Text = primeraFecha.ToString();
+                tbSegundaFecha.Text = segundaFecha.ToString();
+                tbTerceraFecha.Text = terceraFecha.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private bool evaluarFechasVacias()
+        {
+            bool fechasVacias = false;
+            if (dpPrimeraFechaEdicion.Text == "")
+            {
+                fechasVacias = true;
+            }
+            if (dpSegundaFechaEdicion.Text == "")
+            {
+                fechasVacias = true;
+            }
+            if (dpTerceraFechaEdicion.Text == "")
+            {
+                fechasVacias = true;
+            }
+            return fechasVacias;
+        }
+
+        private async void modificarFechasTutorias(PeriodoEscolar periodoEscolar)
+        {
+            var conexionServicio = new ServiciosTutorias.Service1Client();
+            if (conexionServicio != null)
+            {
+                ResultadoOperacion resultado = await conexionServicio.registrarFechaSesiontutoriaAsync(periodoEscolar);
+                if (resultado.Error == false)
+                {
+                    MessageBox.Show("Modificacion de fechas exitosa", "Modificacion exitosa", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Modificacion de fechas fallida", "Error en la modificacion", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Modificacion de fechas fallida", "Error en la modificacion", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
     }
 }
